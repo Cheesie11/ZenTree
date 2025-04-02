8709a41a41c0df70d5ac290107a80817ac13be13
Cheesie <115614743+Cheesie11@users.noreply.github.com>
2025-04-02 11:39:50 +0200

    css adjustments and the creation of the new section: library

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,26 @@
-import { Image, StyleSheet } from "react-native";
+import { Image, SafeAreaView, StyleSheet, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
   return (
-    <ThemedView style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <ThemedText type="title" style={styles.title}>
         ZenTree
       </ThemedText>
-      <ThemedText style={styles.subtitle}>
+      <ThemedText style={styles.text}>
         Track, nurture, and never forget to water your bonsai.
       </ThemedText>
       <Image
         source={require("@/assets/images/home-img.jpg")}
         style={styles.welcomeImage}
       />
-    </ThemedView>
+      <View style={styles.libraryContainer}>
+        <ThemedText style={styles.librarySubtitle}>
+          Your Library:
+        </ThemedText>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -23,29 +28,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "flex-start", 
-    paddingTop: 100,
-    paddingHorizontal: 20,
+    justifyContent: "flex-start",
     backgroundColor: "white",
   },
   title: {
-    fontSize: 28,
-    fontWeight: "bold",
+    fontSize: 35,
     marginBottom: 5,
+    marginTop: 20,
     textAlign: "center",
     fontFamily: "IndieFlower",
+    lineHeight: 55,
     color: '#73906e',
   },
-  subtitle: {
+  text: {
     fontSize: 16,
     textAlign: "center",
     marginBottom: 20,
-    color: "black",
+    color: "#333",
     fontFamily: "IndieFlower",
+    paddingHorizontal: 25,
   },
   welcomeImage: {
     width: 400,
     height: 300,
     resizeMode: "contain",
+  },
+  libraryContainer: {
+    width: '100%',
+    alignItems: 'flex-start',
+    paddingHorizontal: 25,
+    marginTop: 20,
+  },
+  librarySubtitle: {
+    fontSize: 22,
+    fontWeight: "600",
+    color: "#333",
+    fontFamily: "IndieFlower",
   },
 });
