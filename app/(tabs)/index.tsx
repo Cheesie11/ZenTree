c3e79e6b68ec19174bb78fbdad6518f951d95123
Cheesie <115614743+Cheesie11@users.noreply.github.com>
2025-04-02 10:47:34 +0200

    formatted the document, additionally found a different icon for the info page and adjusted the position of the content on the home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,21 @@
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title" style={styles.title}>ZenTree</ThemedText>
+      <ThemedText type="title" style={styles.title}>
+        ZenTree
+      </ThemedText>
       <ThemedText style={styles.subtitle}>
         Track, nurture, and never forget to water your bonsai.
       </ThemedText>
-      <Image source={require('@/assets/images/home-img.jpg')} style={styles.welcomeImage} />
+      <Image
+        source={require("@/assets/images/home-img.jpg")}
+        style={styles.welcomeImage}
+      />
     </ThemedView>
   );
 }
@@ -18,27 +23,28 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: 'white',
+    alignItems: "center",
+    justifyContent: "flex-start", 
+    paddingTop: 100,
+    paddingHorizontal: 20,
+    backgroundColor: "white",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
-    textAlign: 'center',
-    color: 'black'
+    textAlign: "center",
+    color: "black",
   },
   subtitle: {
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 20,
-    color: 'black'
+    color: "black",
   },
   welcomeImage: {
     width: 400,
     height: 300,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
 });
